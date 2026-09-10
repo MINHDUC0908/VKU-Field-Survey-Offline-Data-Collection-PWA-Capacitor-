bffb53915b4c6a66133484cd4e38eb991a99f038
duc <ducle090891999@gmail.com>
2026-09-10 14:36:24 +0700

    Đóng gói APK

---
+++ android/app/src/main/java/vn/edu/vku/facilityinspector/MainActivity.java
@@ -0,0 +1,5 @@
+package vn.edu.vku.facilityinspector;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'vn.edu.vku.facilityinspector',
+  appName: 'VKU Facility Inspector',
+  webDir: 'dist'
+};
+
+export default config;
